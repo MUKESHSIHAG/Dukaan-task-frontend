24979241e2b5134e2b67b4a06bd4c197c89cb100
mukeshsihag <sihagmukesh22@gmail.com>
2020-06-18 18:00:03 +0530

    added build

---
+++ build/precache-manifest.0672d890f9978bf0b633033b9899a296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811a662460707eec9cf1e95c8c219a65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb582b4e0523dd1e9d7",
+    "url": "/static/css/2.8f4b44ec.chunk.css"
+  },
+  {
+    "revision": "ceb582b4e0523dd1e9d7",
+    "url": "/static/js/2.f8dd1a2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f8dd1a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620c52346340ad2d65eb",
+    "url": "/static/js/main.e52b1253.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
